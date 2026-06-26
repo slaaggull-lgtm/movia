3e507363a72cbe105f56e26a2436c81e4d2c5548
Sıla Ağgül <slaaggull@gmail.com>
2026-06-26 10:36:52 +0300

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
           <Route path="/register" element={<Register />} />
 
           <Route path="/mood" element={<ProtectedRoute guestAllowed><Mood /></ProtectedRoute>} />
-          <Route path="/quiz" element={<ProtectedRoute><Quiz /></ProtectedRoute>} />
+          <Route path="/quiz" element={<ProtectedRoute guestAllowed><Quiz /></ProtectedRoute>} />
           <Route path="/recommendations" element={<ProtectedRoute guestAllowed><Recommendations /></ProtectedRoute>} />
           <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
           <Route path="/stats" element={<ProtectedRoute><Stats /></ProtectedRoute>} />
